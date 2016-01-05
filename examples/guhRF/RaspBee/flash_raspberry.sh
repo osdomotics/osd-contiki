74145cf90dbf527d2356bdc72ae6e1672e9b995f
Boernsman <bernhard.trinnes@guh.guru>
2016-01-06 00:26:09 +0100

    updated raspberry pi flash skript

--- examples/guhRF/RaspBee/flash_raspberry.sh
+++ examples/guhRF/RaspBee/flash_raspberry.sh
@@ -1,22 +1,53 @@
 #!/bin/bash
 
-echo "killing tunslip"
-sudo killall tunslip6
+set -e
+#set -x
 
-if which gpio > /dev/null; then
-	echo "wiringPi exist" 
-else
-	echo "installing wiringPi"
-	git clone git://git.drogon.net/wiringPi
-	cd wiringPi
-	./build
+BASH_GREEN="\e[1;32m"
+BASH_RED="\e[1;31m"
+BASH_NORMAL="\e[0m"
+
+function printGreen(){
+ echo -e "${BASH_GREEN}$1${BASH_NORMAL}"
+}
+
+if [ "$UID" -ne 0 ]; then
+ echo -e "${BASH_RED}Please start the script as root${BASH_NORMAL}"
+ exit
 fi
 
-sudo gpio mode 0 out
-sudo gpio write 0 0
+printGreen "Stop tunslib6..."
+
+sudo systemctl stop tunslib6
+
+if [ ! -d /sys/class/gpio/gpio18 ]; then
+ printGreen "Export GPIO18"
+ sudo echo "18" > /sys/class/gpio/export
+fi
+
+if [ ! -d /sys/class/gpio/gpio17 ]; then
+ printGreen "Export GPIO17"
+ sudo echo "17" > /sys/class/gpio/export
+fi
+
+printGreen "Set GPIO17 out"
+sudo echo "out" > /sys/class/gpio/gpio17/direction
+printGreen "Set GPIO17 low"
+sudo echo "0" > /sys/class/gpio/gpio17/value
+
+printGreen "Set GPIO18 out"
+sudo echo "out" > /sys/class/gpio/gpio18/direction
+printGreen "Set GPIO18 low"
+sudo echo "0" > /sys/class/gpio/gpio18/value
 sleep 1
-sudo gpio write 0 1
+printGreen "Set GPIO17 high"
+sudo echo "1" > /sys/class/gpio/gpio17/value
 
-sudo avrdude -pm256rfr2 -c stk500 -P/dev/ttyAMA0 -b57600 -D -U flash:w:border-router.RaspBee.hex:a -U eeprom:w:border-router.RaspBee.eep:a
+printGreen "Flash firmware"
+sudo avrdude -pm256rfr2 -c stk500 -P/dev/ttyAMA0 -b57600 -D -U flash:w:border-router.RaspBee.hex:a -U eeprom:w:border-router.RaspBee.eep:a || true
 
-echo "Now you can run tunslip6.sh again"
+printGreen "Set GPIO18 high"
+sudo echo "1" > /sys/class/gpio/gpio18/value
+
+printGreen "Start tunslib6"
+sudo systemctl restart tunslib6
