bc50edd0f2c49905d2ced4d21bbd1da80e024ece
Boernsman <bernhard.trinnes@guh.guru>
2016-01-06 13:56:28 +0100

    added Contiki reset skript

--- examples/guhRF/RaspBee/reset_raspberry.sh
+++ examples/guhRF/RaspBee/reset_raspberry.sh
@@ -20,11 +20,6 @@ printGreen "Stop tunslib6..."
 
 sudo systemctl stop tunslib6
 
-if [ ! -d /sys/class/gpio/gpio18 ]; then
- printGreen "Export GPIO18"
- sudo echo "18" > /sys/class/gpio/export
-fi
-
 if [ ! -d /sys/class/gpio/gpio17 ]; then
  printGreen "Export GPIO17"
  sudo echo "17" > /sys/class/gpio/export
@@ -35,19 +30,10 @@ sudo echo "out" > /sys/class/gpio/gpio17/direction
 printGreen "Set GPIO17 low"
 sudo echo "0" > /sys/class/gpio/gpio17/value
 
-printGreen "Set GPIO18 out"
-sudo echo "out" > /sys/class/gpio/gpio18/direction
-printGreen "Set GPIO18 high"
-sudo echo "1" > /sys/class/gpio/gpio18/value
 sleep 1
+
 printGreen "Set GPIO17 high"
 sudo echo "1" > /sys/class/gpio/gpio17/value
-
-printGreen "Flash firmware"
-sudo avrdude -pm256rfr2 -c stk500 -P/dev/ttyAMA0 -b57600 -D -U flash:w:border-router.RaspBee.hex:a -U eeprom:w:border-router.RaspBee.eep:a || true
-
-printGreen "Set GPIO18 low"
-sudo echo "0" > /sys/class/gpio/gpio18/value
 
 printGreen "Start tunslib6"
 sudo systemctl restart tunslib6
