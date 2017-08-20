edc7a59091459c3ebf1eb62728360ea19eeb0f66
Ralf Schlatterbeck <rsc@runtux.com>
2017-08-20 20:57:17 +0200

    New make environment for new bootloader
    Compatible with old bootloader. Adds an additional section with a copy
    of the interrupt vector table to the end of the image. This is needed by
    the new bootloader and should be ok for the old bootloader.
    
    Note that for this to work, everybody needs python installed with
    the IntelHex python package. On Linux this can be achieved with
    
    pip install IntelHex

--- tools/genbackupisr.py
+++ tools/genbackupisr.py
@@ -10,6 +10,5 @@ import intelhex
 ihex = intelhex.IntelHex (sys.argv [1])
 bin  = ihex.tobinstr () [:0x200]
 
-hex2 = intelhex.IntelHex ()
-hex2.puts (0x1ed00, bin)
-hex2.tofile ("out.hex", format="hex")
+ihex.puts (0x1ed00, bin)
+ihex.tofile (sys.stdout, format="hex")
