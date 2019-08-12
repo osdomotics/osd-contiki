9ab598aaa7f5cd6c8f0aaf9334cb0b6736cc252f
Harald Pichler <harald@the-develop.net>
2019-08-12 23:16:56 +0200

    initial upload

---
+++ core/sys/time.h
@@ -0,0 +1,25 @@
+/**
+ * \defgroup Time related functions
+ *
+ * This rolls the necessary definition for getting/setting time and
+ * managing local time into one include file, on posix systems this
+ * lives in at least two include files, time.h and sys/time.h
+ *
+ * @{
+ */
+
+/**
+ * \file
+ *         Definitions for the time module
+ *      
+ * \author
+ *         Ralf Schlatterbeck <rsc@tux.runtux.com>
+ */
+
+#ifndef time_h
+#define time_h
+
+#include "xtime.h"
+
+#endif // time_h
+/** @} */
